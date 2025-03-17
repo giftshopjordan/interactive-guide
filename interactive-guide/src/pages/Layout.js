282306cf53069e2e6f269bfa85ee341533f95c53
giftshopjordan <jordan.gauvreau@nbrhc.on.ca>
2025-03-17 14:11:30 +0000

    Created

--- interactive-guide/src/pages/Layout.js
+++ interactive-guide/src/pages/Layout.js
@@ -9,10 +9,18 @@ const Layout = () => {
                         <Link to="/">Home</Link>
                     </li>
                     <li>
-                        
+                        <Link to="/cheques">Cheques</Link>
+                    </li>
+                    <li>
+                        <Link to="/clover">Clover</Link>
+                    </li>
+                    <li>
+                        <Link to="/quickbooks">Quickbooks</Link>
                     </li>
                 </ul>
             </nav>
+
+            <Outlet />
         </>
     )
 }
