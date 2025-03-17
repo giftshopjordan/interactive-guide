cfdc757b9339e6316a3065e40ea32490aadfad85
giftshopjordan <jordan.gauvreau@nbrhc.on.ca>
2025-03-17 13:59:33 +0000

    Set up folder structure

---
+++ interactive-guide/src/pages/Layout.js
@@ -0,0 +1,18 @@
+import { Outlet, Link} from "react-router-dom";
+
+const Layout = () => {
+    return (
+        <>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        
+                    </li>
+                </ul>
+            </nav>
+        </>
+    )
+}
