e2a7dfa644b0efcc0f7b2fc74b95b9dce790a9f2
giftshopjordan <jordan.gauvreau@nbrhc.on.ca>
2025-03-17 19:26:56 +0000

    fixed

--- interactive-guide/src/index.js
+++ interactive-guide/src/index.js
@@ -6,7 +6,7 @@ import Clover from "./pages/Clover";
 import Cheques from "./pages/Cheques"
 import Quickbooks from "./pages/Quickbooks";
 import NoPage from "./pages/NoPage";
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 export default function App() {
   return (
@@ -26,8 +26,3 @@ export default function App() {
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
