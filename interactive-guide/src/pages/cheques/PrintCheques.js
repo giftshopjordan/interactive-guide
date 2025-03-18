0fbf8ad64c1aea6b087a6806b10d2ad1a3c6ce8f
giftshopjordan <jordan.gauvreau@nbrhc.on.ca>
2025-03-18 19:34:08 +0000

    Added more steps

--- interactive-guide/src/pages/cheques/PrintCheques.js
+++ interactive-guide/src/pages/cheques/PrintCheques.js
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
-import payBills from "../../images/pay_bills.png";
-import payBillScreen from "../../images/pay_bills_screen.png";
-import invoiceCheck from "../../images/invoice_check.png";
-import creditUsed from "../../images/credits_used.png";
-import setCredit from "../../images/set_credits.png";
 
+const imageContext = require.context('../../images', false, /\.(png|jpe?g|gif)$/);
+const images = imageContext.keys().reduce((acc, path) => {
+    const imageName = path.replace('./', '').replace(/\.(png|lpe?g|gif)$/, '');
+    acc[imageName] = imageContext(path);
+    return acc;
+}, {});
 
 const PrintCheques = () => {
     return <>
@@ -19,23 +20,33 @@ const PrintCheques = () => {
                                 Gather all the invoices from the blue <b>Gift Shop Invoices</b> folder. Make sure they are all <Link to="/quickbooks/PayBills">entered into QuickBooks</Link>.
                             </li>
                             <li>
-                                Once in Quickbooks, select <b>Pay Bills</b> from the <b>Home</b> window. <img src={payBills} alt="Pay bills icon" />
+                                Once in Quickbooks, select <b>Pay Bills</b> from the <b>Home</b> window. <img class="infopic" src={images['pay_bills']} alt="Pay bills icon" />
                             </li>
                             <li>
-                                Verify with Quickbooks that you have entered all the invoices. <img src={payBillScreen} alt="Pay bills screen"/>
+                                Verify with Quickbooks that you have entered all the invoices. <img class="infopic" src={images['pay_bills_screen']} alt="Pay bills screen"/>
                             </li>
                         </ul>
                     </li>
                     <li>
                         <ul>
                             <li>
-                                Select every invoice to be paid using the check mark box on the left side. <img src={invoiceCheck} alt="Invoice check mark" />
+                                Select every invoice to be paid using the check mark box on the left side. <img class="infopic" src={images['invoice_check']} alt="Invoice check mark" />
                             </li>
                             <li>
-                                If there are any credits available, apply the credits <b>only</b> if they are less than the invoice amount. <img src={setCredit} alt="Credits used" />
+                                If there are any credits available, apply the credits <b>only</b> if they are less than the invoice amount. <img class="infopic" src={images['set_credits']} alt="Credits used" />
                             </li>
                             <li>
-                                Click <b>Set Credits</b> to apply. <img src={creditUsed} alt="Amount to pay." />
+                                Click <b>Set Credits</b> to apply. <img class="infopic" src={images['credits_used']} alt="Amount to pay." />
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <ul>
+                            <li>
+                                Make sure to change the date (in the bottom left of the screen) to <b>today’s date</b>. <img class="infopic" src={images['bills_pay_date']} alt="Today's date" />
+                            </li>
+                            <li>
+                                Select <b>Pay Selected Bills</b>. <img class="infopic" src={images['pay_selected_bills']} alt="Selected bills button" />
                             </li>
                         </ul>
                     </li>
